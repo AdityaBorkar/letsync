75ca00efb80916b3a737f4d68b5e7e4c44655b14
Aditya Borkar <aditya.borkar.programs@gmail.com>
2024-12-02 23:09:06 +0530

    fix: actions version

--- packages/core/src/schema/builder-sql.ts
+++ packages/core/src/schema/builder-sql.ts
@@ -1,9 +1,7 @@
 import type { Config } from '@/types/index.js';
 
-type ok = {};
-
 export function SchemaBuilder_SQL(schema: Config['schema']) {
 	// ...
 	console.log({ schema });
 }
-// TODO - MAKE THE STEPS OF CONTRIBUTING SO SIMPLE THAT EVERYTHING CAN BE OPERATED VIA THE CLI AND NO OPENING OF UI IS REQURIED
+// TODO - MAKE THE STEPS OF CONTRIBUTING SO SIMPLE THAT EVERYTHING CAN BE OPERATED VIA THE CLI AND NO OPENING OF UI IS REQUIRED
